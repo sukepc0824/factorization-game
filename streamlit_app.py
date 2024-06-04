b46d3b95f70faceed4c638d16fe9f73cab272914
pcsuke <110383951+sukepc0824@users.noreply.github.com>
2024-06-04 20:57:00 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,276 @@
-# Streamlitライブラリをインポート
-import streamlit as st
+<!DOCTYPE html>
+<html>
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Inter:wght@100..900&display=swap');
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+        body {
+            width: 100%;
+            height: 500px;
+            margin: auto;
+            position: relative;
+            background: #f3f3f3;
+            font-family: "DM Mono", sans-serif;
+            color: #222;
+        }
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+        dialog {
+            border-radius: 10px;
+            border: #ddd;
+            text-align: center;
+            padding: 12px;
+            width: 300px;
+            font-family: "Inter", sans-serif;
+        }
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
+        dialog h2 {
+            font-size: 32px;
+            margin: 24px;
+        }
 
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
+        dialog .score-wrap {
+            margin: 32px;
+        }
 
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
+        dialog p {
+            margin: 0;
+        }
 
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
+        dialog .score {
+            font-size: 70px;
+            font-family: "DM Mono", sans-serif !important;
+        }
 
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+        dialog .high-score {
+            font-size: 32px;
+        }
+
+        dialog button {
+            border: none;
+            width: 100%;
+            font-size: 22px;
+            padding: 4px;
+            border-radius: 5px;
+        }
+
+        header {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            display: flex;
+            height: 36px;
+            top: 15px;
+            align-items: center;
+            justify-content: center;
+            gap: 0px;
+            width: 540px;
+        }
+
+        header .status {
+            width: 50%;
+            padding: 0 20px;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            gap: 4px;
+            border-radius: 400px;
+        }
+
+        header progress {
+            width: 100%;
+            accent-color: #222;
+            font-size: 13px;
+        }
+
+        header .status-score {
+            width: 56px;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            font-size: 23px;
+            border: #ccc 1px solid;
+            border-radius: 400px;
+            color: #ef7000;
+        }
+
+        header .time {
+            width: 40px;
+            text-align: center;
+            font-size: 26px;
+        }
+
+        .output {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        h1 {
+            text-align: center;
+            font-weight: 500;
+            font-size: 80px;
+            margin: 0;
+            animation: animationZoom 26s linear forwards;
+        }
+
+        @keyframes animationZoom {
+            100% {
+                transform: scale(2);
+            }
+        }
+
+        .container {
+            position: absolute;
+            left: 50%;
+            bottom: 0;
+            transform: translateX(-50%);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
+            gap: 5px;
+            width: 100%;
+        }
+
+        .container button {
+            background-color: white;
+            border: #ddd solid 1px;
+            width: 78px;
+            padding: 20px;
+            font-size: 34px;
+            font-family: "DM Mono", sans-serif;
+            border-radius: 10px;
+            color: #222;
+        }
+
+        button {
+            outline: none;
+        }
+
+        button:hover {
+            filter: brightness(94%);
+        }
+    </style>
+</head>
+
+<body>
+    <dialog>
+        <div class="score-wrap">
+            <p>score:</p>
+            <p class="score">0</p>
+        </div>
+        <form method="dialog">
+            <button onclick="location.reload()">Play Again</button>
+        </form>
+    </dialog>
+    <header>
+        <div class="status">
+            <p>TIME:</p>
+            <progress value="0" max="30"></progress>
+        </div>
+        <div class="status-score">
+            0
+        </div>
+    </header>
+    <div class="output">
+        <h1></h1>
+    </div>
+    <div class="container">
+    </div>
+
+    <script>
+        let primes = [2, 3, 5, 7, 11, 13]
+        let prime_list = [2, 3, 5, 7]
+        let prime_number = 1
+        let difficulty = 3
+        let score = 0
+
+        function set_timer() {
+            let countDownSeconds = 30
+            document.querySelector("progress").value = countDownSeconds
+            countdown = setInterval(function () {
+                countDownSeconds--
+
+                document.querySelector("progress").value = countDownSeconds
+
+                if (countDownSeconds === 0) {
+                    clearInterval(countdown)
+                    gameover()
+                }
+            }, 1000);
+        }
+
+        function gameover() {
+            document.querySelector("dialog").showModal()
+            document.querySelector(".score").innerText = score
+        }
+
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max)
+        }
+
+        function generate_product() {
+            set_timer()
+            for (let i = 0; i < difficulty ** 2 / 6; i++) {
+                prime_number *= prime_list[Math.floor(Math.random() * (prime_list.length - 1) + 1)] * (Math.round(getRandomInt(difficulty) + 1))
+            }
+            document.querySelector("h1").remove()
+
+            let element_button = document.createElement("h1")
+            element_button.innerText = prime_number
+            document.querySelector(".output").append(element_button)
+        }
+
+        function create_buttons() {
+            document.querySelector(".container").innerHTML = ''
+            prime_list.forEach((value, index) => {
+                let element_button = document.createElement("button")
+                element_button.innerText = value
+                element_button.setAttribute("onclick", `devide(${value})`)
+                document.querySelector(".container").append(element_button)
+            })
+        }
+
+        function devide(number) {
+            if (prime_number % number == 0) {
+                prime_number /= number
+                score += number
+                document.querySelector(".status-score").innerText = score
+
+                document.querySelector("h1").innerText = prime_number
+
+                if (prime_number === 1) {
+                    difficulty += 0.25
+                    if (Number.isInteger(difficulty)) {
+                        document.querySelector("h1").innerText = "!"
+                        if (primes[difficulty] != undefined) {
+                            prime_list.push(primes[difficulty])
+                            create_buttons()
+                        }
+                    } else {
+                        document.querySelector("h1").innerText = "1"
+                    }
+
+                    clearInterval(countdown)
+
+                    window.setTimeout(generate_product, 340);
+                }
+            } else {
+                gameover()
+            }
+        }
+        create_buttons()
+        generate_product()
+    </script>
+</body>
+
+</html>
